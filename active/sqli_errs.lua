851c0ae6b90c75ecab592f26cdafc216d19cac65
Khaled Nassar <knassar702@gmail.com>
2023-01-03 00:26:41 +0200

    Mutli-threading for sqli

--- active/sqli_errs.lua
+++ active/sqli_errs.lua
@@ -1,4 +1,4 @@
-SQLI_ERRORS = read(string.format("%s/txt/sqli_errs.txt",SCRIPT_PATH))
+SQLI_ERRORS = read(JOIN_SCRIPT_DIR("txt/sqli_errs.txt"))
 
 PAYLOADS = {
     "'123",
@@ -28,33 +28,29 @@ local function send_report(url,parameter,payload,matching_error)
     print_report(VulnReport)
 end
 
-function main(url) 
-    local resp = http:send("GET",HttpMessage:getUrl())
-    if resp.errors:GetErrorOrNil() then
-        local log_msg = string.format("[SQLI_ERRORS] Connection Error: %s",new_url)
-        log_error(log_msg)
-        return
-    end
-    for param_index, param_name in pairs(HttpMessage:getParams()) do
-        STOP_PARAM = false
-        for payload_index, payload in pairs(PAYLOADS) do 
-            local new_url = HttpMessage:setParam(param_name,payload)
-            local resp = http:send("GET",new_url)
-            local body = resp.body:GetStrOrNil()
-            if STOP_PARAM == true then
-                break
-            end
-            for sqlerror_match in SQLI_ERRORS:gmatch("[^\n]+") do
-                    local match = is_match(sqlerror_match,body)
-                    if ( match == false or match == nil) then
-                            -- NOTHING
-                    else
-                        send_report(resp.url:GetStrOrNil(),param_name,payload,sqlerror_match)
-                        Reports:addVulnReport(VulnReport)
-                        STOP_PARAM = true
-                        break
-                    end
-            end
+function matcher(param_name)
+    STOP_PARAM = false
+    for payload_index, payload in pairs(PAYLOADS) do 
+        local new_url = HttpMessage:setParam(param_name,payload)
+        local resp = http:send("GET",new_url)
+        local body = resp.body:GetStrOrNil()
+        if STOP_PARAM == true then
+            break
+        end
+        for sqlerror_match in SQLI_ERRORS:gmatch("[^\n]+") do
+                local match = is_match(sqlerror_match,body)
+                if ( match == false or match == nil) then
+                        -- NOTHING
+                else
+                    send_report(resp.url:GetStrOrNil(),param_name,payload,sqlerror_match)
+                    Reports:addVulnReport(VulnReport)
+                    STOP_PARAM = true
+                    break
+                end
         end
     end
+end
+
+function main(url) 
+    LuaThreader:run_scan(HttpMessage:getParams(), matcher, 30)
 end
