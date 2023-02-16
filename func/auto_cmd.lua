e86dda6bd4c78b4f1ffe540b4fd77f8a6737e2b7
Khaled Nassar <knassar702@gmail.com>
2023-02-16 03:35:49 +0200

    adding a few lua CVE

--- func/auto_cmd.lua
+++ func/auto_cmd.lua
@@ -21,42 +21,126 @@ function print_report(report)
 end
 
 -- Sending http request
+-- * `method` http method
+-- * `url` target url
+-- * `body` request body
+-- * `headers` request headesr in table
+--
+-- Example:
+--
+-- ```lua
+-- local status, resp = pcall(function() 
+--      return http:send("GET","http://example.com/",nil,{"X-API": "123"})
+--end)
+--
+-- ```
 function http.send(self,method, url,body, headers)
 
 end
+
+
+-- Set Custom http Proxy url for the script
+-- * `proxy_url` String
+--
+-- Example: 
+--
+-- `http:set_proxy('http:set_proxy('http://localhost:8080/')')`
+function http.set_proxy(self, proxy_url)
     
+end
+
+-- Set the max http redirects
+-- * `many_redirects` integer
+-- Example: 
+--
+-- `http:set_redirects(3)`
+function http.set_redirects(self,many_redirects)
+    
+end
+    
+
+-- Set CVE Report Risk
+-- * `setRisk` ["high","medium","low"]
+function CveReport:setRisk(self,risk)
+    
+end
+
+
+-- Set CVE Report Name
+-- * `setName` String
+function CveReport:setName(self, setName)
+    
+end
+
+
+-- Set CVE Report Description
+-- * `setDescription` String
+function CveReport:setDescription(self, description)
+    
+end
+
+-- is this text matched with Regex Pattern
+-- * `text` String
+-- * `regex` String
+function is_match() end
+
+-- Print The CveReport in the console
+-- * `CVE_REPORT` - CveReport Class
+function print_cve_report(CVEREPORT)end
+
+-- Saving the CVE Report in the JSON output
+-- * `CVE_REPORT` - CveReport Class
+function Reports:addCveReport(self, CVEREPORT)end
+
+-- Saving the VULN Report in the JSON output
+-- * `VULN_REPORT` - VulnReport Class
+function Reports:addVulnReport(self, VULNREPORT)end
+
+-- Add CVE Report Matchers
+-- You can use this with multiaple lines to add all matches in one list
+--```lua
+--  CveReport:addMatcher("MATCHED_DATA", MATCH_ID)
+--
+--```
+-- 1 = RawResponse (full response)
+--
+--
+-- 2 = Response Headers
+--
+--
+-- 3 = Reponse Body
+--
+--
+-- 4 = Status Code
+--
+--
+-- above 4 = General
+function CveReport:addMatcher(self,match_string, match_id)end
+
+-- Set CVE Report URL
+-- * `url` String
+function CveReport:setUrl(self, url)end
 
 -- Print new line above the progress bar
-function println(text)
-end
+function println(text)end
     
--- Delay
-function sleep(delay_time)
-end
+-- Set Delay for the script
+function sleep(delay_time)end
 
 -- reading files
-function read(file_path)
-end
+function read(file_path)end
 
 -- error log
-function log_error(txt)
-    
-end
+function log_error(txt)end
 
 -- info log
-function log_info(txt)
-    
-end
+function log_info(txt)end
 
 -- warn log
-function log_warn(txt)
-    
-end
+function log_warn(txt)end
 
 -- debug log
-function log_debug(txt)
-    
-end
+function log_debug(txt)end
 
 
 -- Regex matching
@@ -83,3 +167,29 @@ function html_search(html_data, css_pattern)
 end
 
 
+
+-- Making sure that the response body matches the list by the `and` condition
+-- * `body` String
+-- * `text_list` table
+function ResponseMatcher:match_body(self,body, txt) end
+
+
+-- is the string startswith X or not
+-- * `txt` - String
+-- * `txt2` - String
+-- Example
+--
+-- ```lua
+-- str_startswith("Hello World","Hello")
+--```
+function str_startswith(text, txt2)end
+
+-- is the string contains X or not
+-- * `txt` - String
+-- * `txt2` - String
+-- Example
+--
+-- ```lua
+-- str_contains("Hello World, WE ARE USING LUA FOR WEB Security YAYYYYYYy","USING")
+--```
+function str_contains(text,txt2)end
